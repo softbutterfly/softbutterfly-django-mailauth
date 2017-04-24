ea31bd601144d1ce1367741f85b814515d63dbc9
zodiacfireworks <martin.vuelta@gmail.com>
2017-04-24 18:46:33 -0500

    update to be compilant with django 1.10.x

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import find_packages
 
 
 PACKAGE_NAME = 'softbutterfly-django-mailauth'
-PACKAGE_VERSION = '3.14.18'
+PACKAGE_VERSION = '3.14.19'
 PACKAGE_LIST = find_packages()
 REQUERIMENTS = [
     'django',
