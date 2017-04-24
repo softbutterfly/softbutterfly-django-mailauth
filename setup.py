79dc651f2946cf90ca6aa68241a06d80180b4dbd
zodiacfireworks <martin.vuelta@gmail.com>
2017-04-24 18:28:31 -0500

    update setup and requeriment

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import find_packages
 
 
 PACKAGE_NAME = 'softbutterfly-django-mailauth'
-PACKAGE_VERSION = '3.14.17'
+PACKAGE_VERSION = '3.14.18'
 PACKAGE_LIST = find_packages()
 REQUERIMENTS = [
     'django',
