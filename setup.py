c2126cb98f0f38011b30feefaed3a47bfd9f49a1
zodiacfireworks <martin.vuelta@gmail.com>
2017-04-24 13:40:48 -0500

    update setup information and prepare script

--- setup.py
+++ setup.py
@@ -1,31 +1,15 @@
 from setuptools import setup
-from setuptools import find_packages
-
-from codecs import open
-from os import path
-
-here = path.abspath(path.dirname(__file__))
-
-with open(path.join(here, 'README.md'), encoding='utf-8') as f:
-    long_description = f.read()
 
 
 setup(
     name='softbutterfly-django-mailauth',
-
-    version='0.0.0',
-
+    version='1.0.0',
     description='A drop-in module for users indentified by email.',
-    long_description=long_description,
-
-    author='softbutterfly',
+    author='SoftButterfly',
     author_email='dev@softbutterfly.io',
-
     license='BSD',
-
     url='https://github.com/softbutterfly/softbutterfly-django-mailauth',
     download_url='https://github.com/softbutterfly/softbutterfly-django-mailauth/tarball/1.0.0',
-
     classifiers=[
         'Development Status :: 5 - Production/Stable',
         'Environment :: Web Environment',
@@ -35,21 +19,12 @@ setup(
         'Framework :: Django',
         'Programming Language :: Python',
     ],
-
+    zip_safe=False,
     keywords=['softbutterfly', 'django', 'auth', 'mail as username'],
-
-    zip_safe=True,
-
-    packages=find_packages(),
+    packages=[
+        'softbutterfly.mailauth',
+    ],
     include_package_data=True,
-
-
-    package_data={
-        'softbutterfly': [
-        ],
-    },
-
-
     install_requires=[
         'django',
     ],
